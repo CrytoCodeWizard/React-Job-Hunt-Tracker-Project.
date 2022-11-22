9a9bae2680e58edc22fdf5fb83d941c3ff4202ba
David Teo <starrynightskyhoney@gmail.com>
2022-11-22 17:17:39 -0300

    Add database connection

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,11 +1,26 @@
 import express from "express";
 
+import { connectToDB } from "./database/connect";
+import { settings } from "./config/settings";
+
 const app = express();
+
+const run = async () => {
+  try {
+    await connectToDB();
+
+    console.log("Connected to the database.");
+
+    app.listen(settings.port, () => {
+      console.log(`Server is running on port ${settings.port}.`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 app.get("/", (req, res) => {
   res.send("API is running");
 });
 
-app.listen(5000, () => {
-  console.log("Server is running on port 5000");
-});
+run();
