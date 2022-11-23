54ffed2b029f89edcb62dc5f9a822142e6816cb3
David Teo <starrynightskyhoney@gmail.com>
2022-11-23 17:53:04 -0300

    Update auth.ts in utilities, add BEARER_JWT

--- backend/src/utilities/auth.ts
+++ backend/src/utilities/auth.ts
@@ -1,9 +1,28 @@
 import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
+
 import { settings } from "../config/settings";
+import { JwtUserPayload } from "../interfaces/JwtUserPayload";
 
 export const hashPassword = async (password: string) =>
   await bcrypt.hash(password, await bcrypt.genSalt());
 
-export const createJWT = (object: object) =>
+export const passwordCheck = async (
+  password: string,
+  correctPassword: string
+) => {
+  return await bcrypt.compare(password, correctPassword);
+};
+
+export const createJWT = (object: JwtUserPayload) =>
   jwt.sign(object, settings.jwtSecret);
+
+export const verifyJWT = (token: string) => {
+  return jwt.verify(token, settings.jwtSecret);
+};
+
+export const getJwtFromHeaders = (headers: string) => {
+  return headers.split(" ")[1];
+};
+
+export const BEARER_JWT = "Bearer ";
