c48e2786e997c7c3f9515b3aab60d748f1e55c88
David Teo <starrynightskyhoney@gmail.com>
2022-11-24 15:57:01 -0300

    Update main.ts

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,13 +1,16 @@
 import express from "express";
+import cors from "cors";
+import helmet from "helmet";
+import rateLimit from "express-rate-limit";
+
+import authRouter from "./routes/auth";
+import jobsRouter from "./routes/jobs";
 
 import { connectToDB } from "./database/connect";
 import { settings } from "./config/settings";
 
 import { notFoundMiddleware } from "./middleware/not-found";
 import { errorHandlerMiddleware } from "./middleware/error-handler";
-
-import authRouter from "./routes/auth";
-import jobsRouter from "./routes/jobs";
 import { authMiddleware } from "./middleware/auth-handler";
 
 const run = async () => {
@@ -18,7 +21,21 @@ const run = async () => {
 
     const app = express();
 
+    app.set("trust proxy", 1);
+    app.disable("x-powered-by");
+
+    app.use(
+      "/api",
+      rateLimit({
+        windowMs: 15 * 60 * 1000, // 15 minutes
+        max: 100,
+        standardHeaders: true,
+        legacyHeaders: false,
+      })
+    );
     app.use(express.json());
+    app.use(cors());
+    app.use(helmet());
 
     app.use("/api/v1/auth", authRouter);
     app.use("/api/v1/jobs", authMiddleware, jobsRouter);
