3ab86f34a5b445671509fbaa8fcd2e93a64bfb76
David Teo <starrynightskyhoney@gmail.com>
2022-11-27 19:12:20 -0300

    Add PasswordField component

---
+++ frontend/src/components/PasswordField.tsx
@@ -0,0 +1,60 @@
+import { forwardRef, useRef } from "react";
+import { HiEye, HiEyeOff } from "react-icons/hi";
+import {
+  FormControl,
+  FormLabel,
+  IconButton,
+  Input,
+  InputGroup,
+  InputProps,
+  InputRightElement,
+  useDisclosure,
+  useMergeRefs,
+} from "@chakra-ui/react";
+
+const PasswordField = forwardRef<HTMLInputElement, InputProps>((props, ref) => {
+  const { isOpen, onToggle } = useDisclosure();
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  const mergeRef = useMergeRefs(inputRef, ref);
+
+  const onClickReveal = () => {
+    onToggle();
+    if (inputRef.current) {
+      inputRef.current.focus({ preventScroll: true });
+    }
+  };
+
+  return (
+    <FormControl>
+      <FormLabel htmlFor="password">Password</FormLabel>
+      <InputGroup>
+        <InputRightElement>
+          <IconButton
+            variant="link"
+            aria-label={isOpen ? "Mask password" : "Reveal password"}
+            icon={isOpen ? <HiEyeOff /> : <HiEye />}
+            onClick={onClickReveal}
+          />
+        </InputRightElement>
+        <Input
+          borderColor="gray.400"
+          _hover={{
+            borderColor: "gray.500",
+          }}
+          id="password"
+          ref={mergeRef}
+          name="password"
+          type={isOpen ? "text" : "password"}
+          autoComplete="current-password"
+          required
+          {...props}
+        />
+      </InputGroup>
+    </FormControl>
+  );
+});
+
+PasswordField.displayName = "PasswordField";
+
+export default PasswordField;
