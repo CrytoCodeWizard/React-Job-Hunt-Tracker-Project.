774c0a0442519c4d69210ab64c07308e52973cb8
David Teo <starrynightskyhoney@gmail.com>
2022-11-29 18:13:48 -0300

    Remove Grid and use Flex in JobList

--- frontend/src/components/Job/JobList.tsx
+++ frontend/src/components/Job/JobList.tsx
@@ -1,4 +1,4 @@
-import { Grid, GridItem } from "@chakra-ui/react";
+import { Box, Flex, Grid, GridItem } from "@chakra-ui/react";
 import { Dispatch } from "react";
 import { JobsAPIResponse } from "../../api/interfaces/jobs";
 import Job from "./Job";
@@ -14,25 +14,13 @@ interface JobListProps {
 
 const JobList = ({ jobs, editState }: JobListProps) => {
   return (
-    <Grid
-      p={5}
-      as={"ul"}
-      templateColumns={[
-        "repeat(1, 1fr)",
-        "repeat(2, 1fr)",
-        "repeat(4, 1fr)",
-        "repeat(5, 1fr)",
-      ]}
-      justifyContent="space-around"
-      placeItems={["center", "unset"]}
-      gap={8}
-    >
+    <Flex wrap={"wrap"} p={5} as={"ul"} gap={8}>
       {jobs.map((item, index) => (
-        <GridItem key={item._id}>
+        <Box margin={"auto"} key={item._id}>
           <Job job={item} index={index + 1} editState={editState} />
-        </GridItem>
+        </Box>
       ))}
-    </Grid>
+    </Flex>
   );
 };
 
