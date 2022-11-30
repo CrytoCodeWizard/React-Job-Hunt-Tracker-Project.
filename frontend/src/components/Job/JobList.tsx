9398c235de2479c2330a893f1a1d55900598aa6f
David Teo <starrynightskyhoney@gmail.com>
2022-11-30 08:32:26 -0300

    Add animations and update styles

--- frontend/src/components/Job/JobList.tsx
+++ frontend/src/components/Job/JobList.tsx
@@ -1,7 +1,9 @@
-import { Box, Flex, Grid, GridItem } from "@chakra-ui/react";
+import { Box, Flex } from "@chakra-ui/react";
 import { Dispatch } from "react";
 import { JobsAPIResponse } from "../../api/interfaces/jobs";
 import Job from "./Job";
+import { useAutoAnimate } from "@formkit/auto-animate/react";
+import useAppStore from "../../store/store";
 
 interface JobListProps {
   jobs: JobsAPIResponse[];
@@ -13,10 +15,18 @@ interface JobListProps {
 }
 
 const JobList = ({ jobs, editState }: JobListProps) => {
+  const [parent] = useAutoAnimate<HTMLInputElement>();
+  const jobsState = useAppStore((state) => state.jobs);
+
   return (
-    <Flex wrap="wrap" p={5} as="ul" gap={8}>
+    <Flex ml={[0, 3]} ref={parent} wrap="wrap" p={5} gap={8}>
       {jobs.map((item, index) => (
-        <Box borderRadius="xl" overflow="auto" margin="auto" key={item._id}>
+        <Box
+          borderRadius="xl"
+          overflow="auto"
+          margin={["auto", "auto", jobsState.length === 1 ? "center" : 0]}
+          key={item._id}
+        >
           <Job job={item} index={index + 1} editState={editState} />
         </Box>
       ))}
--- frontend/src/layouts/AppLayout.tsx
+++ frontend/src/layouts/AppLayout.tsx
@@ -1,10 +1,19 @@
 import { Box, BoxProps } from "@chakra-ui/react";
+import { useAutoAnimate } from "@formkit/auto-animate/react";
 
 type AppLayoutProps = BoxProps;
 
 const AppLayout = ({ children }: AppLayoutProps) => {
+  const [parent] = useAutoAnimate<HTMLInputElement>();
+
   return (
-    <Box h="100vh" background={"gray.100"} overflowY="auto">
+    <Box
+      ref={parent}
+      h="100vh"
+      background={"gray.100"}
+      overflowY="auto"
+      overflowX="hidden"
+    >
       {children}
     </Box>
   );
