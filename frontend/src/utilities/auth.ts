f969f43d3cc8848cc69c301243eca701678d025a
David Teo <starrynightskyhoney@gmail.com>
2022-12-07 10:37:26 -0300

    Fix verifyUser function

--- frontend/src/utilities/auth.ts
+++ frontend/src/utilities/auth.ts
@@ -1,9 +1,9 @@
-import { AuthAPI } from "../api/auth";
 import { localStorageKeys } from "../api/config";
+import { JobsAPI } from "../api/jobs";
 
 export const verifyUser = async () => {
   try {
-    const response = await AuthAPI.verifyJwt();
+    const response = await JobsAPI.findAll();
     if (response.status === 200) {
       return true;
     }
