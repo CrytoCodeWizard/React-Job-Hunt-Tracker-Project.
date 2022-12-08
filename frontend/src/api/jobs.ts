f91e4a92d17975438333e03870283e1576def5be
David Teo <starrynightskyhoney@gmail.com>
2022-12-08 06:05:41 -0300

    Add deleteAll static function and refactor JobsAPI

--- frontend/src/api/jobs.ts
+++ frontend/src/api/jobs.ts
@@ -7,44 +7,31 @@ import { JobsAPIResponse } from "./interfaces/jobs";
 
 const myAxios = axios.create({
   baseURL: settings.jobsApiUrl,
+  headers: {
+    Authorization: getJwtTokenFromLocalStorage(),
+  },
 });
 
 export class JobsAPI {
   static async findAll() {
-    return await myAxios.get<JobsAPIResponse[]>(`${settings.jobsPath}`, {
-      headers: {
-        Authorization: getJwtTokenFromLocalStorage(),
-      },
-    });
+    return await myAxios.get<JobsAPIResponse[]>(`${settings.jobsPath}`);
   }
   static async findOne(id: string) {
-    return await myAxios.get<JobsAPIResponse>(`${settings.jobsPath}/${id}`, {
-      headers: {
-        Authorization: getJwtTokenFromLocalStorage(),
-      },
-    });
+    return await myAxios.get<JobsAPIResponse>(`${settings.jobsPath}/${id}`);
   }
   static async create(data: Jobs) {
-    return await myAxios.post<JobsAPIResponse>(`${settings.jobsPath}`, data, {
-      headers: {
-        Authorization: getJwtTokenFromLocalStorage(),
-      },
-    });
+    return await myAxios.post<JobsAPIResponse>(`${settings.jobsPath}`, data);
   }
 
   static async delete(id: string) {
-    return await myAxios.delete(`${settings.jobsPath}/${id}`, {
-      headers: {
-        Authorization: getJwtTokenFromLocalStorage(),
-      },
-    });
+    return await myAxios.delete(`${settings.jobsPath}/${id}`);
+  }
+
+  static async deleteAll() {
+    return await myAxios.delete(`${settings.jobsPath}`);
   }
 
   static async update(id: string, data: Jobs) {
-    return await myAxios.patch(`${settings.jobsPath}/${id}`, data, {
-      headers: {
-        Authorization: getJwtTokenFromLocalStorage(),
-      },
-    });
+    return await myAxios.patch(`${settings.jobsPath}/${id}`, data);
   }
 }
